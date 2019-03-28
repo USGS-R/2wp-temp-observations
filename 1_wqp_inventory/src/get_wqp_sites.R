23e34195111ba304c7f413ca12198641a82ea6fa
Oliver <soliver@usgs.gov>
2019-03-28 15:22:48 -0500

    fixed args in whatWQPsites

--- 1_wqp_inventory/src/get_wqp_sites.R
+++ 1_wqp_inventory/src/get_wqp_sites.R
@@ -5,8 +5,8 @@ get_wqp_sites <- function(wqp_params) {
   all_vars <- unlist(wqp_params$characteristicName)
   
   all_sites <- whatWQPsites(siteType = wqp_params$Sitetype, 
-                           samplemedia = wqp_params$sampleMedia, 
-                           characteristicname = all_vars)
+                           sampleMedia = wqp_params$sampleMedia, 
+                           characteristicName = all_vars)
 
   return(all_sites)
 }
