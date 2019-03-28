934966dfb62f996b77b0e9ed832165d2352c55c4
Oliver <soliver@usgs.gov>
2019-03-28 12:17:54 -0500

    generalize site summaries so can handle lakes and streams seperately.

--- 1_wqp_inventory/src/summarize_sites.R
+++ 1_wqp_inventory/src/summarize_sites.R
@@ -1,47 +1,36 @@
-make_site_summaries <- function(wqp_sites) {
+summarize_sites <- function(wqp_sites, site_key) {
   
-  county_summ <- wqp_sites %>%
-    group_by(StateCode, CountyCode) %>%
-    summarize(n_sites = n()) %>%
-    left_join(dataRetrieval::countyCd, by = c('StateCode' = 'STATE', 'CountyCode' = 'COUNTY'))
-  
-  state_summ_streams <- wqp_sites %>%
-    filter(grepl('stream|river', MonitoringLocationTypeName, ignore.case = TRUE)) %>%
+  state_summ <- wqp_sites %>%
+    filter(grepl(site_key, MonitoringLocationTypeName, ignore.case = TRUE)) %>%
     group_by(StateCode) %>%
     summarize(n_sites = n()) %>%
     left_join(dataRetrieval::stateCd, by = c('StateCode' = 'STATE')) %>%
     mutate(region = tolower(STATE_NAME)) %>%
     filter(!is.na(region))
+ 
+  return(state_summ)
   
-  state_summ_lakes <- wqp_sites %>%
-    filter(grepl('lake|reserv|impound', MonitoringLocationTypeName, ignore.case = TRUE)) %>%
-    group_by(StateCode) %>%
-    summarize(n_sites = n()) %>%
-    left_join(dataRetrieval::stateCd, by = c('StateCode' = 'STATE')) %>%
-    mutate(region = tolower(STATE_NAME)) %>%
-    filter(!is.na(region))
-  
-  library(maps)
-  library(ggplot2)
-  library(ggthemes)
-  
-  us_states_stream <- map_data('state') %>%
-    left_join(state_summ_streams)
-  
-  us_states_lakes <- map_data('state') %>%
-    left_join(state_summ_lakes)
+}
 
-  ggplot(data = us_states_stream, 
-         mapping = aes(x = long, y = lat, group = group)) +
-    geom_polygon(aes(fill = n_sites), color = "gray70", size = 0.1) +
-    coord_map(projection = "albers", lat0 = 39, lat1 = 45) +
-    scale_fill_gradient(low = "cornsilk", high = "#CB454A") +
-    theme_map()
+plot_summary <- function(summary_in, file_out) {
+  us_states_sites <- map_data('state') %>%
+    left_join(summary_in)
   
-  ggplot(data = us_states_lakes, 
-         mapping = aes(x = long, y = lat, group = group)) +
+  
+  wrapper <- function(x, ...) {
+    paste(strwrap(x, ...), collapse = "\n")
+  }
+  
+  keyword <- ifelse(grepl('lake', file_out), 'lake', 'stream')
+  titletext <- paste('Number of unique', keyword, 'sites with water temperature observations in WQP')
+  
+  p <- ggplot(data = us_states_sites, 
+              mapping = aes(x = long, y = lat, group = group)) +
     geom_polygon(aes(fill = n_sites), color = "gray70", size = 0.1) +
     coord_map(projection = "albers", lat0 = 39, lat1 = 45) +
     scale_fill_gradient(low = "cornsilk", high = "#CB454A") +
-    theme_map()
+    theme_map() +
+    labs(title = wrapper(titletext, width = 60),
+         fill = 'Sites')
+  ggsave(file_out, p, width = 6, height = 4)
 }
