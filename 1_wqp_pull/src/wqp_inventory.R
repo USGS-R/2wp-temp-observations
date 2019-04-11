4424314a8e34113b3373ce6b4c142016d36e8e84
Oliver <soliver@usgs.gov>
2019-04-11 16:33:54 -0500

    get rid of both constituent partitioning and archive for partitions. We're wiping clean each time we want new data (in time, or new constituents)

--- 1_wqp_pull/src/wqp_inventory.R
+++ 1_wqp_pull/src/wqp_inventory.R
@@ -70,31 +70,24 @@ inventory_wqp <- function(inv_ind, wqp_pull_params) {
 #' @param wqp_partition_config YAML file containing an element called
 #'   $target_pull_size, giving the maximum number of records that should be in a
 #'   single call to WQP.
-#' @param archive_ind Name of the indicator file for the current archive of
-#'   partitions information, which will be modified as a side effect of this
-#'   function
 #' @return Nothing useful is returned; however, this function (1) Writes to
 #'   partitions_ind a table having the same number of rows as wqp_needs - e.g.,
 #'   for each site/variable combination. The dataframe stores the number of
 #'   observations for that site/variable in WQP and the unique task identifier
 #'   that partitions site/variables into WQP pulls.
-partition_wqp_inventory <- function(partitions_ind, inventory_ind, wqp_partition_cfg, archive_ind) {
+partition_wqp_inventory <- function(partitions_ind, inventory_ind, wqp_partition_cfg) {
   # Read in the inventory, crosswalk & config
   wqp_inventory <- feather::read_feather(sc_retrieve(inventory_ind))
   wqp_partition_config <- yaml::yaml.load_file(wqp_partition_cfg)
 
-  partitions <- bind_rows(lapply(unique(wqp_inventory$ParamGroup), function(constituent) {
-
-    # Filter the inventory to rows for this constituent
-    constit_inventory <- wqp_inventory %>%
-      filter(ParamGroup == constituent)
+  
 
     # Define the atomic_groups to use in setting up data pull partitions. An
     # atomic group is a combination of parameters that can't be reasonably split
     # into multiple WQP pulls. We're defining the atomic groups as distinct
     # combinations of constituent (a group of characteristicNames) and NHD-based
     # site ID
-    atomic_groups <- constit_inventory %>%
+    atomic_groups <- wqp_inventory %>%
       #group_by(site_id) %>%
       #summarize(LakeNumObs=sum(resultCount)) %>%
       arrange(desc(resultCount))
@@ -118,7 +111,7 @@ partition_wqp_inventory <- function(partitions_ind, inventory_ind, wqp_partition
       { ceiling(sum(.)/target_pull_size) }
     num_partitions <- n_single_site_partitions + n_multi_site_partitions
 
-    # Assign each site (or lake) to a partition. Sites/lakes with huge numbers
+    # Assign each site to a partition. Sites with huge numbers
     # of observations will each get their own partition.
     partition_sizes <- rep(0, num_partitions)
     assignments <- rep(NA, nrow(atomic_groups)) # use a vector rather than adding a col to atomic_groups b/c it'll be way faster
@@ -129,7 +122,7 @@ partition_wqp_inventory <- function(partitions_ind, inventory_ind, wqp_partition
       partition_sizes[smallest_partition] <- partition_sizes[smallest_partition] + size_i
     }
 
-    # Prepare one data_frame containing info about each site and lake, including
+    # Prepare one data_frame containing info about each site, including
     # the pull, constituent, and task name (where task name will become the core
     # of the filename)
     pull_time <- Sys.time()
@@ -138,24 +131,11 @@ partition_wqp_inventory <- function(partitions_ind, inventory_ind, wqp_partition
     partitions <- atomic_groups %>%
       mutate(
         PullDate = pull_time,
-        ParamGroup = constituent,
-        PullTask = sprintf('%s_%s_%03d', constituent, pull_id, assignments)) %>%
-      left_join(select(constit_inventory, MonitoringLocationIdentifier, SiteNumObs=resultCount), by='MonitoringLocationIdentifier') %>%
+        PullTask = sprintf('%s_%s_%03d', pull_id, assignments)) %>%
+      left_join(select(wqp_inventory, MonitoringLocationIdentifier, SiteNumObs=resultCount), by='MonitoringLocationIdentifier') %>%
       select(MonitoringLocationIdentifier, SiteNumObs, PullTask, PullDate, ParamGroup)
 
     return(partitions)
-  }))
-
-  # As a side effect, append this data_frame to an archive file that records
-  # all planned partitions. We will likely end up with too many such files
-  # while testing the partitioning/pulling code, but we'd rather have too many
-  # than risk having to dig through the data files to recover a mapping of
-  # which sites have data in which files.
-  archive_file <- sc_retrieve(archive_ind)
-  partitions_archive <- feather::read_feather(archive_file)
-  partitions_all <- bind_rows(partitions_archive, partitions)
-  feather::write_feather(partitions_all, archive_file)
-  gd_put(archive_ind) # 1-arg version requires scipiper 0.0.11+
 
   # Also write the data_frame to a location that will get overwritten with
   # each new pass through this function
@@ -163,48 +143,3 @@ partition_wqp_inventory <- function(partitions_ind, inventory_ind, wqp_partition
   gd_put(partitions_ind) # 1-arg version requires scipiper 0.0.11+
 }
 
-#' Create and push an empty partitions archive if needed. If a partitions
-#' archive already exists, this function does nothing.
-#'
-#' @param archive_ind the .ind file for the desired partitions archive feather
-#'   file
-initialize_partitions_archive <- function(archive_ind) {
-  # only write the file if it doesn't already exist
-  if(file.exists(archive_ind)) {
-    message('partitions archive already exists; skipping initialization')
-    return()
-  }
-
-  empty_archive <-
-    data_frame(
-      MonitoringLocationIdentifier='',
-      SiteNumObs=0L,
-      PullTask='',
-      PullDate=Sys.time(),
-      ParamGroup='') %>%
-    filter(FALSE)
-  archive_file <- scipiper::as_data_file(archive_ind)
-  feather::write_feather(empty_archive, path=archive_file)
-  gd_put(archive_ind, archive_file)
-}
-
-# not ready for prime time:
-#' #' This function can be manually run every so often to clean out partitions
-#' #' records for data pulls that never happened. When running this, be careful to ...
-#' clean_partitions_archive <- function(archive_ind, partitions_file) {
-#'
-#'   # read in the current (pending) partitions file and ...
-#'   partitions_current <- feather::read_feather(partitions_file)
-#'
-#'   # retrieve and read in the archive file
-#'   archive_file <- sc_retrieve(archive_ind)
-#'   partitions_archive <- feather::read_feather(archive_file)
-#'
-#'   # clean up the archive file by comparing to ...
-#'
-#'
-#'   # save and push the archive file back to the shared cache
-#'   feather::write_feather(partitions_all, archive_file)
-#'   gd_put(archive_ind, archive_file)
-#'
-#' }
