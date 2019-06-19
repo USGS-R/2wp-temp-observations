bc04202eb44dcb9271a883862801e21c842345c9
Hines <mhines@usgs.gov>
2019-06-19 16:21:39 -0500

    Use dist2Line function, subset points by reach layer extent

--- getNearestSegment.R
+++ getNearestSegment.R
@@ -1,16 +1,16 @@
 library(scipiper)
 library(data.table)
-library(SearchTrees)
 library(feather)
 library(rgdal)
-library(sp)
 library(geosphere)
+library(dplyr)
+library(sf)
 
 #' @param sites the file path for an inventory file with MonitoringLocationIdentifier, latitude, and longitude at a minimum
 #' @param reaches the file path to a shapefile or gdb with a reach identifier
 #' @param layerName the name of the gdb layer
 #' @param isGDB boolean TRUE if the reaches file is a gdb, FALSE if it is a shapefile
-getNearestSegment <- function(sites, reaches, layerName=NULL, isGDB=FALSE, max_dist=1000){
+getNearestSegment <- function(sites, reaches, layerName=NULL, isGDB=FALSE){
   
   ## read in site data
   featherPath <- gd_get(sites)
@@ -22,38 +22,55 @@ getNearestSegment <- function(sites, reaches, layerName=NULL, isGDB=FALSE, max_d
   } else {
     reachLayer <- readOGR(reaches)
   }
-  
-  ## subset site data to only keep lon, lat, id
-  init_coords <- as.data.table(sitesToMatch)
-  init_coords <- init_coords[, .(lon = longitude, lat = latitude, ID = MonitoringLocationIdentifier)]
-  
-  #project reach layer to wgs84
-  reachLayer <- spTransform(reachLayer, CRS("+proj=longlat +datum=WGS84"))
-  
-  ## extract coordinates from reach shapefile
-  res <- lapply(slot(reachLayer, "lines"), function(x) lapply(slot(x, "Lines"),function(y) slot(y, "coords")))
-  names(res)<-reachLayer@data$seg_id_nat
-  segsID<-sapply(lapply(res,"[[",1),nrow)
-  
-  coords<-as.data.frame(do.call("rbind",lapply(res,"[[",1)),stringsAsFactors=F)
-  colnames(coords)<- c("lon","lat")
-  coords$ID<-rep(names(segsID),segsID)
-  
-  ## create a 'search tree' from coordinates
-  tree <- createTree(coords[,1:2]) # required to calculate the nearest neighbour for each set of coordinates
-  
-  idsList <- vector() # new
-  for (i in 1:nrow(init_coords)){
-    neighbor <- knnLookup(tree,newdat=init_coords[i,1:2],k=1) # lookup nearest neighbour, 'k' determines the number of nearest neighbors
-    
-    D<-coords[neighbor,]
-    D<-as.data.frame(D)
-    D$dist<-geosphere::distm(D[,1:2], init_coords[i,1:2],fun=distVincentyEllipsoid)
 
-    neigh_segm <- D$ID[D$dist<max_dist] # segments with parts below max_dist, new
-    if (length(neigh_segm) == 0) warning(paste("No segments found within the specified maximum distance for ",i,"th coordinate.",sep=""))
-    
-    idsList <- unique(c(idsList,neigh_segm)) # new
+  ## make points spatial so we can subset them by the reach layer
+  projection <- "+proj=longlat +datum=WGS84"
+  siteCoords <- select(sitesToMatch, longitude, latitude) %>%
+    sp::SpatialPoints(proj4string = CRS("+proj=longlat +datum=WGS84")) %>%
+    sp::spTransform(CRS(projection))
+  siteCoordsSPDF <- SpatialPointsDataFrame(siteCoords, proj4string = projection, data=sitesToMatch)
+ 
+  ## project reach layer to wgs84
+  reachLayer <- spTransform(reachLayer, CRS("+proj=longlat +datum=WGS84"))
+
+  ## change reachLayer ID to be the reachId
+  for (i in 1:nrow(reachLayer)) {
+    reachLayer@lines[[i]]@ID <- as.character(reachLayer[i, ]@data[[1]])
   }
-  return(idsList)
+  
+  ## create a bbox around the reaches
+  reachBbox <- st_bbox(reachLayer)
+  reachPoly <- as(raster::extent(reachBbox[[1]],reachBbox[[3]],reachBbox[[2]],reachBbox[[4]]), "SpatialPolygons")
+  
+  ## project poly layer to wgs84
+  crs(reachPoly) <- "+proj=longlat +datum=WGS84"
+  
+  ## geospatially subset points by reachPolygon
+  points_subset <- siteCoordsSPDF[reachPoly,]
+  
+  ## subset to keep only data we need
+  init_coords <- as.data.table(points_subset)
+  init_coords <- init_coords[, .(lon = longitude, lat = latitude)]
+  
+  ## get nearest line to each point
+  dist <- geosphere::dist2Line(p=init_coords, line=reachLayer)
+  dist.df <- as.data.frame(dist)
+  
+  ## get the actual reachId and the original site coords
+  for (i in 1:nrow(dist.df)) {
+    dist.df$reachId[i] <- reachLayer[, ]@lines[[dist.df$ID[i]]]@ID
+    dist.df$site.lat[i] <- init_coords$lat[i]
+    dist.df$site.lon[i] <- init_coords$lon[i]
+  }
+
+  ## get the monitoring Location Identifier from the site coords
+  for (i in 1:nrow(dist.df)) {
+    subset <- sitesToMatch %>% filter(latitude==dist.df$site.lat[i] & longitude==dist.df$site.lon[i])
+    dist.df$monitoringLocationIdentifier[i] <- subset$MonitoringLocationIdentifier                                      
+  }
+  
+  ## remove unnecessary columns
+  dist.df <- select(dist.df, monitoringLocationIdentifier, reachId)
+  
+  return(dist.df)
 }
