5f45e6b09b1a52cf9f9267a5006aab4b6f6e9a39
Sam Oliver <soliver@usgs.gov>
2019-08-22 09:37:45 -0500

    added function to resolve NWIS temperature data, which can be stored in multiple columns. Function choose_temp_column finds the temp column with the most data by site, and preserves those data and drops the rest. Captures which column is being preserved.

--- 2_nwis_pull/src/nwis_pull.R
+++ 2_nwis_pull/src/nwis_pull.R
@@ -74,11 +74,20 @@ combine_nwis_data <- function(ind_file, ...){
   rds_files <- c(...)
   df_list <- list()
 
-  for (i in seq_len(length(rds_files))){
-    df_list[[i]] <- readRDS(rds_files[i])
-  }
 
-  nwis_df <- do.call("rbind", df_list)
+  for (i in seq_len(length(rds_files))){
+    
+    
+    temp_dat <- readRDS(rds_files[i]) 
+    
+    reduced_dat <- choose_temp_column(temp_dat)
+    
+    df_list[[i]] <- reduced_dat
+
+  }
+  
+  nwis_df <- do.call("bind_rows", df_list)
+  
 
   data_file <- scipiper::as_data_file(ind_file)
   saveRDS(nwis_df, data_file)
@@ -127,5 +136,36 @@ get_nwis_data <- function(data_file, partition, nwis_pull_params, service, verbo
   # results because remake expects this function to always create the target
   # file
   saveRDS(nwis_dat, data_file)
+}
+
+choose_temp_column <- function(temp_dat) {
+  # take all temperature columns and put into long df
+  values <- temp_dat %>%
+    select(-ends_with('_cd'), -agency_cd) %>%
+    tidyr::gather(key = 'col_name', value = 'temp_value', -site_no, -dateTime) %>%
+    filter(!is.na(temp_value))
+  
+  # take all temperature cd columns and do the same thing
+  codes <- temp_dat %>%
+    select(site_no, dateTime, ends_with('_cd'), -tz_cd, -agency_cd) %>%
+    tidyr::gather(key = 'col_name', value = 'cd_value', -site_no, -dateTime) %>%
+    mutate(col_name = gsub('_cd', '', col_name)) %>%
+    filter(!is.na(cd_value))
+  
+  # bring together so I have a long df with both temp and cd values
+  all_dat <- left_join(values, codes, by = c('site_no', 'dateTime', 'col_name'))
+  
+  # find which col_name has the most records for each site,
+  # and keep that column
+  top_cols <- all_dat %>%
+    group_by(site_no, col_name) %>%
+    summarize(count_nu = n()) %>%
+    group_by(site_no) %>%
+    slice(which.max(count_nu))
+  
+  # reduce the data down to those site-top col combos
+  reduced_dat <- inner_join(all_dat, select(top_cols, site_no, col_name))
+  
+  return(reduced_dat)
 }
 
