d42c78f2ccc8619f7ee784986e4fef48b54b62aa
wdwatkins <wwatkins@usgs.gov>
2020-07-17 16:04:30 -0700

    up/downstream endpoint calculation working

--- 6_network/src/get_network.R
+++ 6_network/src/get_network.R
@@ -21,6 +21,7 @@ get_national_gf <- function(sb_id, sb_name, out_ind) {
   if(!dir.exists(out_dir)) {
     message("Downloading from Sciencebase...")
     temp_loc <- tempfile()
+    authenticate_sb()
     sbtools::item_file_download(sb_id = sb_id, names = sb_name, destinations = temp_loc)
     if(dir.exists(out_dir)) unlink(out_dir, recursive=TRUE)
     unzip(temp_loc, exdir=dirname(out_dir))
@@ -43,14 +44,12 @@ compute_endpoints <- function(national_network, out_ind) {
   
   # inspect. lessons:
   # Modify reaches according to lessons learned
-  #TODO: other edge cases? is this still relevant w/GFv1.1?
+  #other edge cases? is this still relevant w/GFv1.1?
 
   # Augment gf_reaches with all end points of all reaches. I initially tried to
   # find points in gf_points, but at least one doesn't exist (the outlet to 155)
   # and I realized what I wanted most was the end points anyway.
   #should take about 9 minutes for national, single-threaded
-  #setup_strategy is due to Rstudio bug
-  #https://github.com/rstudio/rstudio/issues/6692
   cl <- makeCluster(detectCores() - 1, setup_strategy = "sequential")
   gf_reaches_indices <- clusterSplit(cl, 1:nrow(gf_reaches))
   gf_reaches_split <- lapply(X = gf_reaches_indices, FUN = function(x) {gf_reaches[x,]})
@@ -61,17 +60,19 @@ compute_endpoints <- function(national_network, out_ind) {
     mutate(end_points = sf::st_cast(Shape, "POINT") %>% {list(c(head(.,1),tail(.,1)))})
   }
   gf_applied <- parLapply(cl, X = gf_reaches_split, fun = add_endpoints)
+  stopCluster(cl)
   gf_reaches_endpoints <- do.call(bind_rows, gf_applied)
   saveRDS(gf_reaches_endpoints, file = as_data_file(out_ind))
   gd_put(out_ind)
 }
 
-compute_up_down_stream <- function(reaches_bounded) {
-  #----------------
-  #~26 minutes for national single-threaded
-  get_reach_direction <- function(nat_seg_ids, reaches_bounded) {
-    `%>%` <- magrittr::`%>%`
-    seg_ids_df <- dplyr::filter(reaches_bounded, seg_id_nat %in% nat_seg_ids)
+compute_up_down_stream <- function(reaches_bounded_ind, out_ind) {
+  
+  reaches_bounded <- readRDS(as_data_file(reaches_bounded_ind)) %>% 
+    ungroup()
+  #~26 minutes for national single-threaded, ~5 minutes on 7 cores
+  get_reach_direction <- function(seg_ids, reaches_bounded) {
+    seg_ids_df <- dplyr::filter(reaches_bounded, seg_id %in% seg_ids)
     reaches_bounded_direction_subset <- seg_ids_df %>%
       dplyr::mutate(
         which_end_up = sapply(seg_id, function(segid) {
@@ -79,29 +80,32 @@ compute_up_down_stream <- function(reaches_bounded) {
           if(!is.na(reach$to_seg)) { # non-river-mouths
             # upstream endpoint is the one furthest from the next reach downstream
             to_reach <- dplyr::filter(reaches_bounded, seg_id == reach$to_seg)
-            which.max(sf::st_distance(reach$end_points[[1]], sf::st_geometry(to_reach)))
+            which.max(sf::st_distance(reach$end_points[[1]], to_reach$Shape))
           } else { # river mouths
             # upstream endpoint is the one closest to a previous reach upstream
-            from_reach <- dplyr::filter(reaches_bounded, to_seg == reach$seg_id)[1,] # only need one
-            which.min(sf::st_distance(reach$end_points[[1]], sf::st_geometry(from_reach)))
+            from_reach <- dplyr::filter(reaches_bounded, to_seg == reach$seg_id) 
+            if(nrow(from_reach) > 0) { 
+              which.min(sf::st_distance(reach$end_points[[1]], from_reach$Shape))
+            } else { #A river mouth with no upstream segment (small coastal streams)
+              NA_integer_  
+            }
           } # turns out that the first point is always the upstream point for the DRB at least. that's nice, but we won't rely on it.
-        }))#,
-      #   up_point = purrr::map2(end_points, which_end_up, function(endpoints, whichendup) { endpoints[[whichendup]] }),
-      #   down_point = purrr::map2(end_points, which_end_up, function(endpoints, whichendup) { endpoints[[c(1,2)[-whichendup]]] })) %>%
-      # dplyr::select(-end_points)
-    return()
+        }),
+        up_point = purrr::map2(end_points, which_end_up, function(endpoints, whichendup) { endpoints[[whichendup]] }),
+        down_point = purrr::map2(end_points, which_end_up, function(endpoints, whichendup) { endpoints[[c(1,2)[-whichendup]]] })) 
+    return(reaches_bounded_direction_subset)
   }
   
-  #timeout is a hack to deal with Rstudio issue with cluster creation
-  #https://github.com/rstudio/rstudio/issues/6692
-  cl <- makeCluster(detectCores() - 1, setup_timeout = 0.5) 
-  nat_seg_ids_split <- clusterSplit(cl, reaches_bounded$seg_id_nat)
-  #clusterExport(cl, varlist = "reaches_bounded", envir = environment())
-  cluster_results <- parLapply(cl = cl, X = nat_seg_ids_split, 
+  cl <- makeCluster(detectCores() - 1)
+  clusterEvalQ(cl, lapply(c('sf', 'dplyr', 'purrr'), library, character.only = TRUE))
+  seg_ids_split <- clusterSplit(cl, reaches_bounded$seg_id)
+  cluster_results <- parLapply(cl = cl, X = seg_ids_split, 
                                fun = get_reach_direction, reaches_bounded)
- 
-  return(reaches_bounded_direction)
-}
+  stopCluster(cl)
+  results_bound <- do.call(bind_rows, cluster_results)
+  saveRDS(results_bound, file = as_data_file(out_ind))
+  gd_put(out_ind)
+} 
 
 # 
 #   # split reaches into segments where necessary so that every pair of points is
