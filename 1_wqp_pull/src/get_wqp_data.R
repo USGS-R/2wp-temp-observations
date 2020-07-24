672f733c7c384908579045f8bb40d5261162a2bb
Samantha Oliver <soliver@usgs.gov>
2020-07-24 09:32:16 -0500

    moved gets to getters.yml, created a dependency on a dummy date for easier/safer rebuilds, and modified calls to whatWQPdata since national pulls no longer work. Now query by state, counties if state fails.

---
+++ 1_wqp_pull/src/get_wqp_data.R
@@ -0,0 +1,151 @@
+# --- functions used in task table ---
+
+# read the partitions file and pull out just those sites that go with a single
+# PullTask and are worth requesting because they have >0 observations for that
+# ParamGroup
+filter_partitions <- function(partitions_ind, pull_task) {
+  partitions <- feather::read_feather(sc_retrieve(partitions_ind, 'getters.yml'))
+  dplyr::filter(partitions, PullTask==pull_task, SiteNumObs > 0)
+}
+
+# pull a batch of WQP observations, save locally, return .tind file
+get_wqp_data <- function(data_file, partition, wqp_pull_params, verbose = TRUE) {
+  
+  # prepare the arguments to pass to readWQPdata
+  wqp_args <- list()
+  wqp_args$characteristicName <- wqp_pull_params$characteristicName$temperature
+  wqp_args$siteid <- partition$MonitoringLocationIdentifier
+  # do the data pull
+  # first pull using readWQPdata, then if that fails, try POST
+  
+  
+  getWQP <- function(wqp_args) {
+    wqp_dat_time <- tryCatch(
+      {
+        time_start <- Sys.time()
+        wqp_dat <- wqp_POST(wqp_args)
+        time_end <- Sys.time()
+        time_diff <- time_end - time_start
+        
+        return(list(time_diff, wqp_dat, 'POST'))
+        
+        
+      },
+      error = function(cond) {
+        message('Call to WQP using POST failed. Trying via readWQPdata. Original error message:')
+        message(cond)
+        
+        time_start <- Sys.time()
+        wqp_dat <- readWQPdata(siteid = wqp_args$siteid,
+                               characteristicName = wqp_args$characteristicName)
+        time_end <- Sys.time()
+        time_diff <- time_end - time_start
+        
+        return(list(time_diff, wqp_dat, 'readWQPdata'))
+      }
+    )
+    return(wqp_dat_time)
+  }
+  
+  wqp_dat_time <- getWQP(wqp_args)
+  
+  # wqp_dat_time <- system.time({
+  #   wqp_dat <- wqp_POST(wqp_args)
+  # })
+  # 
+  # wqp_dr_dat_time <- system.time(dat_dr <- readWQPdata(siteid = wqp_args$siteid,
+  #                                                      characteristicName = wqp_args$characteristicName))
+  if (verbose){
+    message(sprintf(
+      'WQP pull (using %s) for %s took %0.1f %s and returned %d rows',
+      wqp_dat_time[[3]],
+      partition$PullTask[1],
+      as.numeric(wqp_dat_time[[1]]),
+      attr(wqp_dat_time[[1]], 'units'),
+      nrow(wqp_dat_time[[2]])))
+  }
+  # make wqp_dat a tibble, converting either from data.frame (the usual case) or
+  # NULL (if there are no results)
+  wqp_dat <- as_tibble(wqp_dat_time[[2]])
+  
+  # write the data to rds file. do this even if there were 0
+  # results because remake expects this function to always create the target
+  # file
+  saveRDS(wqp_dat, data_file)
+}
+
+# hack around w/ https://github.com/USGS-R/dataRetrieval/issues/434
+wqp_POST <- function(wqp_args_list){
+  wqp_url <- "https://www.waterqualitydata.us/Result/search"
+  
+  
+  wqp_args_list$siteid <- wqp_args_list$siteid
+  post_body = jsonlite::toJSON(wqp_args_list, pretty = TRUE)
+  
+  download_location <- tempfile(pattern = ".zip")
+  pull_metadata <- POST(paste0(wqp_url,"?mimeType=tsv&zip=yes"),
+                        body = post_body,
+                        content_type("application/json"),
+                        accept("application/zip"),
+                        httr::write_disk(download_location))
+  
+  headerInfo <- httr::headers(pull_metadata)
+  unzip_location <- tempdir()
+  unzipped_filename <- utils::unzip(download_location, exdir=unzip_location)
+  unlink(download_location)
+  dat_out <- suppressWarnings(
+    read_delim(
+      unzipped_filename, 
+      col_types = cols(`ActivityStartTime/Time` = col_character(),
+                       `ActivityEndTime/Time` = col_character(),
+                       USGSPCode = col_character(),
+                       ResultCommentText=col_character(),
+                       `ActivityDepthHeightMeasure/MeasureValue` = col_number(),
+                       `DetectionQuantitationLimitMeasure/MeasureValue` = col_number(),
+                       ResultMeasureValue = col_number(),
+                       `WellDepthMeasure/MeasureValue` = col_number(),
+                       `WellHoleDepthMeasure/MeasureValue` = col_number(),
+                       `HUCEightDigitCode` = col_character(), 
+                       `ActivityEndTime/TimeZoneCode` = col_character()),
+      quote = "", delim = "\t"))
+  unlink(unzipped_filename)
+  return(dat_out)
+}
+
+
+# extract and post the data (dropping the site info attribute), creating an
+# .ind file that we will want to share because it represents the shared cache
+combine_wqp_dat <- function(ind_file, ...){
+  
+  rds_files <- c(...)
+  df_list <- list()
+  
+  # create a readRDS function that accomdates some column type issues
+  readRDS2 <- function(.) {
+    dat_mod <- readRDS(.) %>%
+      filter(!is.na(ResultMeasureValue)) %>%
+      select(MonitoringLocationIdentifier, ActivityMediaName, ActivityMediaSubdivisionName, ActivityStartDate, `ActivityStartTime/Time`, `ActivityStartTime/TimeZoneCode`,
+             `ActivityDepthHeightMeasure/MeasureValue`, `ActivityDepthHeightMeasure/MeasureUnitCode`,   
+             `ActivityTopDepthHeightMeasure/MeasureValue`,`ActivityTopDepthHeightMeasure/MeasureUnitCode`, 
+             `ActivityBottomDepthHeightMeasure/MeasureValue`, `ActivityBottomDepthHeightMeasure/MeasureUnitCode`,
+             ActivityCommentText, `SampleCollectionMethod/MethodIdentifier`, `SampleCollectionMethod/MethodIdentifierContext`,
+             `SampleCollectionMethod/MethodName`,ResultDetectionConditionText, CharacteristicName, ResultMeasureValue, 
+             `ResultMeasure/MeasureUnitCode`, MeasureQualifierCode,
+             ResultStatusIdentifier, StatisticalBaseCode, ResultValueTypeName, PrecisionValue, 
+             ResultCommentText, USGSPCode, `ResultDepthHeightMeasure/MeasureValue`, `ResultDepthHeightMeasure/MeasureUnitCode`, ProviderName) %>%
+      mutate(PrecisionValue = as.numeric(PrecisionValue)) %>%
+      mutate_at(vars(contains('MeasureValue')), as.numeric) %>%
+      mutate_if(is.logical, as.character) %>%
+      select_if(~!all(is.na(.)))
+  }
+  
+  for (i in seq_len(length(rds_files))){
+    df_list[[i]] <- readRDS2(rds_files[i])
+  }
+  message('Binding all WQP files.')
+  wqp_df <- do.call("bind_rows", df_list)
+  
+  data_file <- scipiper::as_data_file(ind_file)
+  saveRDS(wqp_df, data_file)
+  gd_put(ind_file, data_file)
+}
--- 1_wqp_pull/src/wqp_inventory.R
+++ 1_wqp_pull/src/wqp_inventory.R
@@ -1,25 +1,66 @@
-#' Get an inventory of the number of records per site/variable combination in
-#' WQP
+#' Create a task table to call whatWQPdata across all states
 #'
 #' Depending on the size of the request, calls to WQP may need to be
 #' partitioned based on record size. This gets the inventory of data available
 #' on WQP that we need to fullfil our "needs" - which is a series of
-#' site/variable combinations.
+#' site/variable combinations. The national pull no longer works, so we 
+#' partition calls to whatWQPdata by state, with an error catch that splits
+#' the pull in half (by counties) if it fails the first time. 
 #'
-#' @param inv_ind Indicator file for a table with columns site and variable
-#'   which describes what we going to pull from WQP, which was calculated as
-#'   the difference between our data "wants" and our data "haves".
-#' @param wqp_pull_params List of lists that contain parameters of interest
-#'   (e.g., temperature) and all corresponding synonyms available in WQP (e.g.,
-#'   "Temperature" and "Temperature, water"), plus other pull parameters.
-#' @param wqp_partition_config configuration information for the data/inventory
-#'   pulls
+#' @param states A states dataframe that includes all states you want to pull, 
+#' and the corresponding counties.
+#' @param final_target Name of output from task table that contains the combined
+#' inventory from all states.
 #' @return A dataframe returned by the function dataRetrieval::whatWQPdata, with
 #'   one row per site/variable combination and the 'resultCount' being the
 #'   variable from which we will make decisions about partitioning data pull
 #'   requests.
-inventory_wqp <- function(inv_ind, wqp_pull_params) {
+do_inventory_tasks <- function(states, final_target, ...) {
   
+  task_name <- unique(states$state_id)
+  
+  # define tasks
+  download_step <- create_task_step(
+    step_name = 'download',
+    target_name = function(task_name, ...) {
+      sprintf('%s_site_inventory', task_name)
+    },
+    command = function(task_name, ...) {
+      sprintf("inventory_wqp(state_id = I('%s'), wqp_pull_params = wqp_pull_parameters)", task_name)
+    }
+  )
+  
+  # create task plan
+  task_plan <- create_task_plan(
+    task_names = task_name, 
+    task_steps = list(download_step),
+    add_complete = FALSE,
+    final_steps = 'download'
+  )
+  
+  # Create the task remakefile
+  create_task_makefile(
+    task_plan = task_plan,
+    makefile = 'wqp_inventory_tasks.yml',
+    include = 'remake.yml',
+    sources = c(...),
+    packages = c('dplyr', 'dataRetrieval', 'xml2'),
+    tickquote_combinee_objects = TRUE,
+    finalize_funs = 'combine_inventory',
+    final_targets = final_target,
+    as_promises = TRUE)
+  
+  # build tasks
+  
+  scmake('wqp_inventory.feather.ind_promise', remake_file='wqp_inventory_tasks.yml')
+
+  
+}
+
+inventory_wqp <- function(state_id, wqp_pull_params) {
+  
+  # function to keep track of time it takes to pull, and 
+  # number of rows in each return
   wqp_call <- function(fun, args) {
     time <- system.time(out <- do.call(fun, args))
     info <- list(
@@ -29,35 +70,81 @@ inventory_wqp <- function(inv_ind, wqp_pull_params) {
     )
     return(info)
   }
-  
   wqp_args <- wqp_pull_params
   
   # collapse all constituents into single vector
   wqp_args$characteristicName <- as.character(unlist(wqp_args$characteristicName))
   
-  # only filter using characteristic names (and huc for test)
-  wqp_dat <- wqp_call(whatWQPdata, wqp_args['characteristicName'])
+  # set state ID
+  wqp_args$statecode <- state_id
+  
+  # Print state-specific message so user can see progress
+  message('Retrieving whatWQPdata for state ', state_id)
+  
+  # first try the full state pull, wrapped in try so function does not fail
+  # with an error. 
+  temp_dat <- try(wqp_call(whatWQPdata, wqp_args[c('characteristicName', 'statecode')]))
+  
+  # catch errors, and break up data into two calls as a backup plan
+  # this splits the call into half by counties
+  if(!'list' %in% class(temp_dat)) {
+    message('State call failed, calling by 1/2 of counties at a time.')
+    state_counties <- filter(dataRetrieval::countyCd, STATE %in% gsub('US:', '', state_id))
+    
+    # split counties in half and make a 2-part call
+    split <- ceiling(nrow(state_counties)/2)
+    
+    # first half of counties
+    wqp_args$countycode <- paste('US', state_counties$STATE, state_counties$COUNTY, sep = ':')[1:split]
+    temp1 <- wqp_call(whatWQPdata, wqp_args[c('characteristicName', 'countycode')])
+    
+    # second half of counties
+    wqp_args$countycode <- paste('US', state_counties$STATE, state_counties$COUNTY, sep = ':')[(split+1):nrow(state_counties)]
+    temp2 <- wqp_call(whatWQPdata, wqp_args[c('characteristicName', 'countycode')])
+    
+    # combine outputs
+    temp_dat <- list(time = temp1$time + temp2$time,
+                     nrow = nrow(temp1$out) + nrow(temp2$out),
+                     out = bind_rows(temp1$out, temp2$out))
+  }
+  
+  # summarize what was retrieved and how long it took
+  message('Retrieved ', temp_dat$nrow, ' rows of data in ', ceiling(temp_dat$time), ' seconds.')
+  
+  return(temp_dat$out)
+}
+combine_inventory <- function(ind_file, ...) {
+  dat_out <- bind_rows(...)
   
   # keep columns of interest, put back lat/long
   # keeping lat/long here in case there was a situation where you'd want to 
   # take stock/map sites before pulling data
-  dat_out <- wqp_dat$out %>%
+  dat_out <- dat_out %>%
     select(OrganizationIdentifier, MonitoringLocationIdentifier, ResolvedMonitoringLocationTypeName, 
            StateName, CountyName, HUCEightDigitCode, latitude = lat, longitude = lon, resultCount)
-  
-  # spit out nrows and time it took to get inventory
-  message(sprintf('sample inventory complete, required %0.2f hours to retrieve %d rows', wqp_dat$time/(60*60), wqp_dat$nrow))
-  
+
   # write and indicate the data file
-  data_file <- scipiper::as_data_file(inv_ind)
+  data_file <- scipiper::as_data_file(ind_file)
   feather::write_feather(dat_out, data_file)
-  gd_put(inv_ind)
+  gd_put(ind_file)
+}
+get_states <- function(states_url) {
   
+  # get state and county codes
+  county_states <- dataRetrieval::countyCd %>%
+    mutate(state_id = paste0('US:', STATE))
+  
+  state_codes <- xml2::read_xml(states_url)
+  states <- unlist(as.character(xml2::xml_children(state_codes)))
+  states <- stringr::str_extract(states, pattern = 'US:\\d{2}')
+  states <- states[!is.na(states)]
+  
+  return(filter(county_states, state_id %in% states))
 }
 
 summarize_wqp_inventory <- function(inv_ind, out_file) {
  
-   wqp_inventory <- feather::read_feather(sc_retrieve(inv_ind))
+   wqp_inventory <- feather::read_feather(sc_retrieve(inv_ind, remake_file = 'getters.yml'))
    
    all <- data.frame(n_sites = nrow(wqp_inventory), 
                      n_records = sum(wqp_inventory$resultCount), stringsAsFactors = FALSE)
@@ -83,10 +170,10 @@ summarize_wqp_inventory <- function(inv_ind, out_file) {
 #'   for each site/variable combination. The dataframe stores the number of
 #'   observations for that site/variable in WQP and the unique task identifier
 #'   that partitions site/variables into WQP pulls.
-partition_wqp_inventory <- function(partitions_ind, wqp_pull_params, inventory_ind, wqp_partition_cfg) {
+partition_wqp_inventory <- function(partitions_ind, wqp_pull_params, inventory_ind, wqp_partition_cfg, pull_date) {
   
   # Read in the inventory & config
-  wqp_inventory <- feather::read_feather(sc_retrieve(inventory_ind))
+  wqp_inventory <- feather::read_feather(sc_retrieve(inventory_ind, 'getters.yml'))
   wqp_partition_config <- wqp_partition_cfg
   
   # filter out site types that are not of interest
@@ -163,12 +250,11 @@ partition_wqp_inventory <- function(partitions_ind, wqp_pull_params, inventory_i
   # Prepare one data_frame containing info about each site, including
   # the pull, constituent, and task name (where task name will become the core
   # of the filename)
-  pull_time <- Sys.time()
-  attr(pull_time, 'tzone') <- 'UTC'
-  pull_id <- format(pull_time, '%y%m%d%H%M%S')
+
+  pull_id <- pull_date
   partitions <- atomic_groups %>%
     mutate(
-      PullDate = pull_time,
+      PullDate = pull_date,
       PullTask = sprintf('%s_%04d', pull_id, assignments)) %>%
     left_join(select(wqp_inventory, MonitoringLocationIdentifier, SiteNumObs=resultCount), by='MonitoringLocationIdentifier') %>%
     select(MonitoringLocationIdentifier, SiteNumObs, PullTask, PullDate)
@@ -183,7 +269,7 @@ partition_wqp_inventory <- function(partitions_ind, wqp_pull_params, inventory_i
 
 summarize_wqp_data <- function(data_ind, out_file) {
   
-  wqp_dat <- readRDS(sc_retrieve(data_ind))
+  wqp_dat <- readRDS(sc_retrieve(data_ind, 'getters.yml'))
   
   wqp_summary <- data.frame(n_obs = nrow(wqp_dat),
                             n_sites = length(unique(wqp_dat$MonitoringLocationIdentifier)))
@@ -191,3 +277,4 @@ summarize_wqp_data <- function(data_ind, out_file) {
   readr::write_csv(wqp_summary, out_file)
   
 }
+
