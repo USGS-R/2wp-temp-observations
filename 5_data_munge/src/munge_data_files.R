c3d28aaf52aaa9dbd0180d8de06de5733a56d7d0
Samantha Oliver <soliver@usgs.gov>
2020-07-30 15:42:10 -0500

    Moved collapse of NWIS uv data to combine step when choosing which temp column to use. UV data was causing memory bog downs when I was trying to filter later on on the bound data. This fixes memory issues, but does not preserve raw data.

--- 5_data_munge/src/munge_data_files.R
+++ 5_data_munge/src/munge_data_files.R
@@ -3,127 +3,26 @@ f_to_c <- function(f) {
   return(c)
 }
 
-munge_wqp_withdepths <- function(in_ind, min_value, max_value, max_daily_range, out_ind) {
-  dat <- readRDS(sc_retrieve(in_ind)) %>%
-    filter(!is.na(`ResultMeasure/MeasureUnitCode`)) %>%
-    filter(ActivityMediaName %in% 'Water') %>%
-    filter(!is.na(`ActivityDepthHeightMeasure/MeasureValue`)|
-                         !is.na(`ResultDepthHeightMeasure/MeasureValue`)|
-                         !is.na(`ActivityTopDepthHeightMeasure/MeasureValue`)|
-                         !is.na(`ActivityBottomDepthHeightMeasure/MeasureValue`)) %>% 
-    filter(ActivityStartDate > as.Date('1800-01-01') & ActivityStartDate < as.Date('2020-01-01'))
-  
-  # create depth column
-  dat <- mutate(dat, sample_depth = case_when(
-    !is.na(`ActivityDepthHeightMeasure/MeasureValue`) ~ `ActivityDepthHeightMeasure/MeasureValue`,
-    is.na(`ActivityDepthHeightMeasure/MeasureValue`) & !is.na(`ResultDepthHeightMeasure/MeasureValue`) ~ `ResultDepthHeightMeasure/MeasureValue`,
-    is.na(`ActivityDepthHeightMeasure/MeasureValue`) & is.na(`ResultDepthHeightMeasure/MeasureValue`) & !is.na(`ActivityTopDepthHeightMeasure/MeasureValue`) ~ `ActivityTopDepthHeightMeasure/MeasureValue`,
-    is.na(`ActivityDepthHeightMeasure/MeasureValue`) & is.na(`ResultDepthHeightMeasure/MeasureValue`) & is.na(`ActivityTopDepthHeightMeasure/MeasureValue`) ~ `ActivityBottomDepthHeightMeasure/MeasureValue`
-  ))
-  
-  dat <- mutate(dat, sample_depth_unit_code = case_when(
-    !is.na(`ActivityDepthHeightMeasure/MeasureValue`) ~ `ActivityDepthHeightMeasure/MeasureUnitCode`,
-    is.na(`ActivityDepthHeightMeasure/MeasureValue`) & !is.na(`ResultDepthHeightMeasure/MeasureValue`) ~ `ResultDepthHeightMeasure/MeasureUnitCode`,
-    is.na(`ActivityDepthHeightMeasure/MeasureValue`) & is.na(`ResultDepthHeightMeasure/MeasureValue`) & !is.na(`ActivityTopDepthHeightMeasure/MeasureValue`) ~ `ActivityTopDepthHeightMeasure/MeasureUnitCode`,
-    is.na(`ActivityDepthHeightMeasure/MeasureValue`) & is.na(`ResultDepthHeightMeasure/MeasureValue`) & is.na(`ActivityTopDepthHeightMeasure/MeasureValue`) ~ `ActivityBottomDepthHeightMeasure/MeasureUnitCode`
-  ))
-  
+munge_nwis <- function(dv_ind, uv_ind, min_value, max_value, out_ind) {
 
-  # now reduce to daily measures
-  dat_reduced <- dat %>%
-    filter(grepl('deg C|deg F', `ResultMeasure/MeasureUnitCode`, ignore.case = TRUE)) %>%
-    mutate(ResultMeasureValue = ifelse(grepl('deg F', `ResultMeasure/MeasureUnitCode`, ignore.case = TRUE),
-                                       f_to_c(ResultMeasureValue), ResultMeasureValue)) %>%
-    filter(ResultMeasureValue > min_value,
-           ResultMeasureValue < max_value) %>%
-    mutate(`ResultMeasure/MeasureUnitCode` = 'deg C')
-    
-  dat_daily <- group_by(dat_reduced, MonitoringLocationIdentifier, ActivityStartDate, sample_depth) %>%
-    summarize(temperature_mean_daily = mean(ResultMeasureValue),
-              activity_start_times = paste(`ActivityStartTime/Time`, collapse = ', '),
-              n_day = n())
+  dv <- readRDS(sc_retrieve(dv_ind, remake_file = 'getters.yml')) %>%
+    mutate(source = 'nwis_dv')
   
-  dat_daily_meta <- select(dat_reduced, -ResultMeasureValue, -`ActivityStartTime/Time`) %>%
-    group_by(MonitoringLocationIdentifier, ActivityStartDate, sample_depth) %>%
-    summarize_all(first)
+  uv <- readRDS(sc_retrieve(uv_ind, remake_file = 'getters.yml')) %>%
+    mutate(source = 'nwis_uv')
   
-  # bring it together
-  dat_daily <- left_join(dat_daily, dat_daily_meta)
-  
-  # save
-  data_file <- scipiper::as_data_file(out_ind)
-  saveRDS(dat_daily, data_file)
-  gd_put(out_ind, data_file)
-    
-}
-
-munge_wqp_withoutdepths <- function(in_ind, min_value, max_value, max_daily_range, out_ind) {
-  dat <- readRDS(sc_retrieve(in_ind)) %>%
-    select(MonitoringLocationIdentifier, ActivityStartDate, ResultMeasureValue, 
-           `ResultMeasure/MeasureUnitCode`, `ActivityStartTime/Time`, ActivityMediaName, 
-           `ActivityDepthHeightMeasure/MeasureValue`, `ResultDepthHeightMeasure/MeasureValue`, 
-           `ActivityTopDepthHeightMeasure/MeasureValue`, `ActivityBottomDepthHeightMeasure/MeasureValue`) %>%
-    filter(!is.na(`ResultMeasure/MeasureUnitCode`)) %>%
-    filter(ActivityMediaName %in% 'Water') %>%
-    filter(is.na(`ActivityDepthHeightMeasure/MeasureValue`)&
-             is.na(`ResultDepthHeightMeasure/MeasureValue`)&
-             is.na(`ActivityTopDepthHeightMeasure/MeasureValue`)&
-             is.na(`ActivityBottomDepthHeightMeasure/MeasureValue`)) %>%
-    filter(ActivityStartDate > as.Date('1800-01-01') & ActivityStartDate < as.Date('2020-01-01'))
-  
- 
-  # now reduce to daily measures
-  # first clean up obviously bad data
-  dat_reduced <- dat %>%
-    filter(grepl('deg C|deg F', `ResultMeasure/MeasureUnitCode`, ignore.case = TRUE)) %>%
-    mutate(ResultMeasureValue = ifelse(grepl('deg F', `ResultMeasure/MeasureUnitCode`, ignore.case = TRUE),
-                                       f_to_c(ResultMeasureValue), ResultMeasureValue)) %>%
-    mutate(`ResultMeasure/MeasureUnitCode` = 'deg C') %>%
-    filter(ResultMeasureValue > min_value, ResultMeasureValue < max_value)
-  
-  dat_daily <- group_by(dat_reduced, MonitoringLocationIdentifier, ActivityStartDate) %>%
-    summarize(temperature_mean_daily = mean(ResultMeasureValue),
-              activity_start_times = paste(`ActivityStartTime/Time`, collapse = ', '),
-              n_day = n())
-  
-  # save
-  data_file <- scipiper::as_data_file(out_ind)
-  saveRDS(dat_daily, data_file)
-  gd_put(out_ind, data_file)
-  
-}
-
-munge_nwis_uv <- function(in_ind, min_value, max_value, max_daily_range, out_ind) {
-  nwis_df <- readRDS(sc_retrieve(in_ind)) %>%
-    filter(!is.na(temp_value), !is.na(dateTime)) %>%
+  nwis <- bind_rows(dv, uv) %>%
     filter(grepl('A', cd_value, ignore.case = FALSE)) %>%
-    filter(temp_value > min_value,
-           temp_value < max_value) %>%
-    mutate(dateTime = as.Date(dateTime)) %>%
-    group_by(site_no, col_name, dateTime) %>%
-    summarize(temperature_mean_daily = round(mean(temp_value), 3), n_day = n())
+    filter(temp_value > min_value & temp_value < max_value)
   
   # save
   data_file <- scipiper::as_data_file(out_ind)
-  saveRDS(nwis_df, data_file)
-  gd_put(out_ind, data_file)
-}
-
-munge_nwis_dv <- function(in_ind, min_value, max_value, max_daily_range, out_ind) {
-
-  dat <- readRDS(sc_retrieve(in_ind)) %>%
-    filter(grepl('A', cd_value, ignore.case = FALSE)) %>%
-    filter(temp_value > min_value,
-           temp_value < max_value)
-  
-  # save
-  data_file <- scipiper::as_data_file(out_ind)
-  saveRDS(dat, data_file)
+  saveRDS(nwis, data_file)
   gd_put(out_ind, data_file)
   
 }
 
-munge_ecosheds <- function(in_ind, out_ind) {
+munge_ecosheds <- function(in_ind, min_value, max_value, out_ind) {
   dat <- readRDS(sc_retrieve(in_ind, remake_file = 'getters.yml'))
   
   # some data appears to be in fahrenheit (values > 45)
@@ -152,32 +51,26 @@ munge_ecosheds <- function(in_ind, out_ind) {
   
 }
 
-combine_all_dat <- function(wqp_ind, nwis_dv_ind, nwis_uv_ind, ecosheds_ind, out_ind) {
-  wqp <- readRDS(sc_retrieve(wqp_ind)) %>%
+combine_all_dat <- function(wqp_ind, nwis_ind, ecosheds_ind, out_ind) {
+  wqp <- readRDS(sc_retrieve(wqp_ind, remake_file = 'getters.yml')) %>%
+    ungroup() %>%
     mutate(date = as.Date(ActivityStartDate), source = 'wqp') %>%
     select(site_id = MonitoringLocationIdentifier, 
            date,
            temp_degC = temperature_mean_daily,
            n_obs = n_day, source)
   
-  nwis_dv <- readRDS(sc_retrieve(nwis_dv_ind)) %>%
-    mutate(date = as.Date(dateTime),
-           source = 'nwiw_dv',
-           site_id = paste0('USGS-', site_no)) %>%
-    select(site_id, date, temp_degC = temp_value, source)
-  
-  nwis_uv <- readRDS(sc_retrieve(nwis_uv_ind)) %>%
-    ungroup() %>%
-    mutate(date = as.Date(dateTime),
-           source = 'nwiw_uv',
-           site_id = paste0('USGS-', site_no)) %>%
-    select(site_id, date, temp_degC = temperature_mean_daily, n_obs = n_day, source)
+  nwis <- readRDS(sc_retrieve(nwis_ind, remake_file = 'getters.yml')) %>%
+    mutate(site_id = paste0('USGS-', site_no)) %>%
+    select(site_id, date = Date, temp_degC = temp_value, n_obs = n, source)
   
   ecosheds <- readRDS(sc_retrieve(ecosheds_ind, remake_file = 'getters.yml')) %>%
-    mutate(source = 'ecosheds') %>%
-    select(site_id = location_id, temp_degC = mean, n_obs = n, source)
+    mutate(source = 'ecosheds',
+           site_id = as.character(location_id)) %>%
+    select(site_id, temp_degC = mean, n_obs = n, source)
   
-  all_dat <- bind_rows(wqp, nwis_dv, nwis_uv, ecosheds)
+  all_dat <- bind_rows(wqp, nwis, ecosheds) %>%
+    distinct(site_id, date, temp_degC, .keep_all = TRUE)
     
   # save
   data_file <- scipiper::as_data_file(out_ind)
@@ -188,24 +81,33 @@ combine_all_dat <- function(wqp_ind, nwis_dv_ind, nwis_uv_ind, ecosheds_ind, out
   
 combine_all_sites <- function(nwis_dv_sites_ind, nwis_uv_sites_ind, wqp_sites_ind, ecosheds_sites_ind, out_ind){
 
-  nwis_sites <- feather::read_feather(sc_retrieve(nwis_dv_sites_ind)) %>%
+  nwis_sites <- feather::read_feather(sc_retrieve(nwis_dv_sites_ind, remake_file = 'getters.yml')) %>%
     mutate(source = 'nwis_dv') %>%
-    bind_rows(feather::read_feather(sc_retrieve(nwis_uv_sites_ind)) %>% 
+    bind_rows(feather::read_feather(sc_retrieve(nwis_uv_sites_ind, remake_file = 'getters.yml')) %>% 
                 mutate(source = 'nwis_uv')) %>%
     mutate(site_id = paste0('USGS-', site_no)) %>%
     select(site_id, site_type = site_tp_cd, latitude = dec_lat_va, longitude = dec_long_va, source)
   
-  wqp_sites <- feather::read_feather(sc_retrieve(wqp_sites_ind)) %>%
+  wqp_sites <- feather::read_feather(sc_retrieve(wqp_sites_ind, remake_file = 'getters.yml')) %>%
     mutate(source = 'wqp') %>%
     select(site_id = MonitoringLocationIdentifier, latitude, longitude, 
            site_type = ResolvedMonitoringLocationTypeName, source)
   
   ecosheds_sites <- readRDS(sc_retrieve(ecosheds_sites_ind, remake_file = 'getters.yml')) %>%
-    mutate(source = 'ecosheds', site_type = 'stream') %>%
-    select(site_id = 'location_id', latitude, longitude, site_type, source)
+    mutate(source = 'ecosheds', site_type = 'stream',
+           site_id = as.character(location_id)) %>%
+    select(site_id, latitude, longitude, site_type, source)
     
   all_sites <- bind_rows(nwis_sites, wqp_sites, ecosheds_sites)
   
   saveRDS(all_sites, as_data_file(out_ind))
   gd_put(out_ind)
+}
+
+summarize_all_dat <- function(in_ind, out_file) {
+  dat_summary <- readRDS(sc_retrieve(in_ind, remake_file = 'getters.yml'))
+  
+  summary <- data.frame(n_obs = nrow(dat_summary), n_sites = length(unique(dat_summary$site_id)))
+  
+  readr::write_csv(summary, out_file)
 }
