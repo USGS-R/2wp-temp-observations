47ce2a76092fdeec28b399d3c4f891cbf22dd866
Atshan <ratshan>
2020-09-04 09:36:28 -0500

    Draft plot to plot the outliers timeseries.

--- 6_data_qaqc.R
+++ 6_data_qaqc.R
@@ -2,6 +2,7 @@ library(scipiper)
 library(dplyr)
 library(tidyverse)
 set.seed(10)
+
 #getting the data from google drive and changing the file to RDS file without ind
 gd_get("5_data_munge/out/daily_temperatures.rds.ind")
 # to get the sites longtitude and latitude data from google drive,
@@ -23,10 +24,12 @@ sites_dat <- readRDS("5_data_munge/out/all_sites.rds") %>%
   # pulling the site_id and latitude bins from the data
 latit_sit_id <- sites_dat %>% select(site_id, bins_lat) %>% distinct()
 
+sites <- unique(daily_dat$site_id) # selecting unique site_id
+site_sub <- sample(sites, 5000)  # selecting sample/subset of sites
 # selecting a random sample from the daily temps data.
 daily_dat_subset <- daily_dat %>%
   filter(!is.na(date)) %>%
-  slice_sample(n = 500000, replace = FALSE) 
+  filter(site_id %in% site_sub) # to select a random subset from daily-temp data
 
 # to find summary and distribution of the subset data.
 summary(daily_dat_subset)
@@ -47,49 +50,57 @@ daily_latit_subset <- left_join(daily_dat_subset, latit_sit_id) %>%
   mutate(flag = ifelse(temp_degC <= low_bound | temp_degC >= up_bound,
                        "o", NA)) 
 
-
 # creating plot to see how is outlier detection is preforming. 
 # 1) creating subset of flagged temps observation. 
 flaged_obs <- daily_latit_subset %>% 
   filter(flag == 'o') 
-sites <- unique(flaged_obs$site_id)
+#sites <- unique(flaged_obs$site_id)
 
-# Finding the site and year most outliers observation from the daily_latitude subset
-max_sit_year <- flaged_obs %>%
-  mutate(year = lubridate::year(date)) %>%
-  group_by(site_id, year ) %>%
+# Taking subset of combined site & latitude data with flagged sites only.
+# Then, finding the site and year most outliers observation,
+max_sit <- flaged_obs %>%
+  group_by(site_id) %>%
   summarize(n_obs = n())
-# to find the 3 maximum of the stie-date subset
-max_3_obs <-  max_sit_year %>%
-  top_n(n =4)
+# to find the 3 maximum of the site-date subset
+max_3_obs <-  max_sit %>%
+  ungroup() %>%
+  slice_max(n_obs, n = 3)
 
+#finding th years with max obs
+max_year <- flaged_obs %>%
+  mutate(year = lubridate::year(date)) %>%
+  group_by(site_id, year) %>%
+  summarize(n_obs = n())
+# to find the 3 maximum of the site-date subset
+max_year <-  max_year %>%
+  ungroup() %>%
+  slice_max(n_obs, n = 3)
 
- 
-for (temp_site in sites) {
-  temp_dat <- flaged_obs %>%
-    (date = as.Date(date)) %>%
-    filter(site_id %in% temp_site) 
-p <- ggplot(flaged_obs, aes(x = month, y = date)) +
-  geom_point() +
-  theme_bw() 
-  #cowplot::theme_cowplot() +
-  #ggtitle(paste0("Timeseries to Detect Outlier: ", temp_site)) 
+temp_year = max_year$year
+
+plot_color = c('black', 'red1')
+ # Looping throgh the sites to plot 
+for (temp_site in max_3_obs$site_id) {
+  temp_dat <- daily_latit_subset %>%
+    group_by(site_id) %>%
+    #filter(!is.na(flag)) %>%
+    filter(site_id %in% temp_site) %>%
+    filter(lubridate::year(date) %in% temp_year) 
+p <- ggplot(temp_dat, aes(x = date, y = temp_degC, colour = flag)) +
+  geom_point(size = 1) +
+  theme_bw() +
+  cowplot::theme_cowplot() +
+  #scale_color_manual(values = plot_color)
+  ggtitle(paste0("Timeseries to Detect Outlier: ", temp_site)) 
   #facet_wrap(~ flag)
 print(p)
 }
-# summarizing the number of flagged temps observations. 
-# creating subset of flagged temps observation. 
-#falged_obs <- daily_latit_subset %>% 
- # length(which(flag == 'o'))
-#  select(flag == 'o', ) %>% unique(seg_id)
 
-
-
-
-#finding the missing data in the date column
+# needed to find the reason for date = NA
+# to do so we looked over the source and site_id 
 mis_dat <- daily_dat %>%
   filter(is.na(date))
-  
+
 site_count <- mis_dat %>%
   group_by(site_id) %>%
   summarize(n_site = n())
@@ -98,6 +109,6 @@ source_count <- mis_dat %>%
   group_by(source) %>%
   summarize(n_source = n())
 # give you rows where date is not NA and where source is Ecosheds
-  filter(!is.na(date) & source %in% 'Ecosheds')
+filter(!is.na(date) & source %in% 'Ecosheds')
   
 
