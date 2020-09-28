d3753d5d2617bc1ade866c8526979993f587c0ab
Atshan <ratshan>
2020-09-28 13:56:21 -0500

    saved

--- 6_QAQC/src/QAQC_Approach2_Site_month_day.R
+++ 6_QAQC/src/QAQC_Approach2_Site_month_day.R
@@ -1,3 +1,9 @@
+library(scipiper)
+library(dplyr)
+library(plotly)
+library(tidyverse)
+set.seed
+
 #creating an r object for the data
 daily_dat <- readRDS('5_data_munge/out/daily_temperatures.rds') %>%
   filter(!is.na(date))
@@ -17,8 +23,7 @@ latit_sit_id <- sites_dat %>% select(site_id, bins_lat) %>% distinct()
 #group by site_ud, source, month, day.
 # cut off line 5 C from mean.
 sites_source_particular_day <- left_join(daily_dat, latit_sit_id) %>%
-  group_by(site_id, source, month = lubridate::month(date),
-           day = lubridate::day(date))%>%
+  group_by(site_id, source, lubridate::yday(date))%>%
   mutate(temp_mean = mean(temp_degC), 
          temp_std = sd(temp_degC),
          outlier_cut_off = ifelse(is.na(temp_std) | (3 * temp_std) < 10 |
@@ -31,46 +36,6 @@ sites_source_particular_day <- left_join(daily_dat, latit_sit_id) %>%
 
 #finding the number of unique sites. 
 sites <- unique(sites_source_particular_day$site_id)
-#finding the number of observation  per site using this grouping. 
-count_site_source_day <- left_join(daily_dat, latit_sit_id) %>%
-  group_by(site_id, source, month = lubridate::month(date),
-           day = lubridate::day(date))%>%
-  summarize(n_per_site = n(),
-            temp_mean = mean(temp_degC), 
-         temp_std = sd(temp_degC),
-         outlier_cut_off = ifelse(is.na(temp_std) | (3 * temp_std) < 10 |
-                                    (3 * temp_std) > 15,  10, 3 * temp_std),
-         low_bound = temp_mean - outlier_cut_off,
-         up_bound = temp_mean + outlier_cut_off) %>%
-  ungroup()
-#
-length(unique(which(count_site_source_day$n_per_site <= 5)))
-# number of sites in our data
-sites <- unique(count_site_source_day$site_id)
-# number of unique sites that used stats to calculate cutoff limit. 
-cal_stat_sit <- sites_source_particular_day %>%
-  filter(!outlier_cut_off %in% 10)
-length(unique(cal_stat_sit$site_id))
-
-# number of sites which have few temperature observation.
-low_obs_site <- count_site_source_day %>%
-  filter(n_per_site <= 1 & source %in% "wqp" & month == 1 & day == 21)
-number_site_month_day <- length(unique(low_obs_site$site_id))
-length(unique(count_site_source_day$site_id))
-
-# finding how many sites required the std replacement. 
-num_std_replacement <- table(count_site_source_day$outlier_cut_off == 10)
-
-# and how many observation does theses sites have. 
-# looking at specific latitude_bin
-jan_40_45_2010 <- sites_source_particular_day %>%
-  filter(bins_lat %in% '(40,45]') %>%
-  filter(month == 1) 
-  
-sample_jan_40_45_2010 <- jan_40_45_2010[sample(nrow(jan_40_45_2010), 50, replace = FALSE, prob = NULL),]
-
-ggplot(sample_jan_40_45_2010, aes(x = site_id, y = temp_degC, color = flag)) +
-  geom_point()
 
 # creating plot to see how is outlier detection is preforming. 
 # 1) creating subset of flagged temps observation. 
@@ -166,3 +131,60 @@ plot_outI <- paste0("6_QAQC/out/USGS-10265150_approachII",'.png')
 ggsave(plot_outI, p_special, height = 5)
 print(p_special)
 ggplotly(p_special)
+
+#finding the number of observation  per site using this grouping. 
+count_site_source_day <- left_join(daily_dat, latit_sit_id) %>%
+  group_by(site_id, source, day_of_year = lubridate::yday(date))%>%
+  summarize(n_per_site = n(),
+            temp_mean = mean(temp_degC), 
+            temp_std = sd(temp_degC),
+            outlier_cut_off = ifelse(is.na(temp_std) | (3 * temp_std) < 10 |
+                                       (3 * temp_std) > 15,  10, 3 * temp_std),
+            low_bound = temp_mean - outlier_cut_off,
+            up_bound = temp_mean + outlier_cut_off) %>%
+  ungroup() %>%
+  mutate(flag = ifelse(temp_degC < low_bound | temp_degC > up_bound,
+                       "o", NA))
+
+  
+
+
+# to find the number of observation and the number of outliers per group. 
+group_flag_count <- sites_source_particular_day %>%
+  select(date, bins_lat, temp_mean, temp_std)  %>%
+  group_by(site_id, source, day_of_year = lubridate::yday(date)) %>%
+  summarize(n_per_site = n()) %>%
+  mutate(n_flagged = ifelse(flag == 'o', n_flagged + 1, n_flagged))
+# to find the number of groups with 1 observation
+groups_1_obs <- filter(count_site_source_day, n_per_site %in% 1) 
+n_groups_1_obs <- nrow(groups_1_obs)
+percent_groups_1_obs <- n_groups_1_obs/nrow(count_site_source_day)
+sites_1_obs <- length(unique(groups_1_obs$site_id))
+
+  
+# number of sites in our data
+sites <- unique(count_site_source_day$site_id)
+# number of unique sites that used stats to calculate cutoff limit. 
+cal_stat_sit <- sites_source_particular_day %>%
+  filter(!outlier_cut_off %in% 10)
+length(unique(cal_stat_sit$site_id))
+
+# number of sites which have few temperature observation.
+low_obs_site <- count_site_source_day %>%
+  filter(n_per_site <= 1 & source %in% "wqp" & month == 1 & day == 21)
+number_site_month_day <- length(unique(low_obs_site$site_id))
+length(unique(count_site_source_day$site_id))
+
+# finding how many sites required the std replacement. 
+num_std_replacement <- table(count_site_source_day$outlier_cut_off == 10)
+
+# and how many observation does theses sites have. 
+# looking at specific latitude_bin
+jan_40_45_2010 <- sites_source_particular_day %>%
+  filter(bins_lat %in% '(40,45]') %>%
+  filter(month == 1) 
+
+sample_jan_40_45_2010 <- jan_40_45_2010[sample(nrow(jan_40_45_2010), 50, replace = FALSE, prob = NULL),]
+
+ggplot(sample_jan_40_45_2010, aes(x = site_id, y = temp_degC, color = flag)) +
+  geom_point()
