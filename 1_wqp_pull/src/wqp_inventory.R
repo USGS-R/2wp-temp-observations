e668c44108f3565ae7b00288580a4fdc0523e579
limnoliver <soliver@usgs.gov>
2022-03-28 15:10:38 -0500

    add another bad org ID to filter out to avoid failed calls to WQP. Closes #51

--- 1_wqp_pull/src/wqp_inventory.R
+++ 1_wqp_pull/src/wqp_inventory.R
@@ -209,7 +209,7 @@ partition_wqp_inventory <- function(partitions_ind, wqp_pull_params, inventory_i
 
   # filter out bad org names
   # these orgs cause "Frequest failed [400]" errors
-  bad_orgs <- grep(' |\\.|/', wqp_inventory$OrganizationIdentifier, value = TRUE)
+  bad_orgs <- grep(" |\\.|/|'", wqp_inventory$OrganizationIdentifier, value = TRUE)
   bad_orgs_sites <- filter(wqp_inventory, OrganizationIdentifier %in% bad_orgs)
 
   if (nrow(bad_orgs_sites) > 0){
